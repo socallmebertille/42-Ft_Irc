b564d90e5d9c37b6a6de606c790e23cb0ec57690
Melinaaam <mmo.melina@gmail.com>
2025-06-03 17:30:51 +0200

    class channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -11,19 +11,19 @@ class Channel
 private:
 	std::map<std::string, Channel>	_channels;
 
-	std::string						_name;
-	std::string						_topic;//cmd topic
-	std::string						_password; //	+k
-	int								_userLimit;//	+l
+	std::string _name;
+	std::string _topic;//cmd topic
+	std::string _password; //	+k
+	int _userLimit;//	+l
 
-	std::set<Client*>				_members;
-	std::set<Client*>				_operators;//+o
-	std::set<Client*>				_invited; // +i
+	std::set<Client*> _members;
+	std::set<Client*> _operators;//+o
+	std::set<Client*> _invited; // +i
 
-	bool							_inviteOnly; // +i
-	bool							_topicRestricted; // +t
-	bool							_passwordProtected; // +k
-	bool							_limitUsers; // +l
+	bool _inviteOnly; // +i
+	bool _topicRestricted; // +t
+	bool _passwordProtected; // +k
+	bool _limitUsers; // +l
 
 public:
 	Channel(const std::string name);
