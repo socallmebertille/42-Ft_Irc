9cd3e4275d3c30c64f0606b223df72ceb1a2eb6a
Melinaaam <mmo.melina@gmail.com>
2025-06-03 17:26:35 +0200

    class channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -1,15 +1,37 @@
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
 
-# include <iostream>
+#include <string>
+#include <map>
+#include <set> // stock sans doublons, et classe les elmts en ordre croissant
 
+class Client;
 class Channel
 {
 private:
-    /* data */
+	std::map<std::string, Channel>	_channels;
+
+	std::string						_name;
+	std::string						_topic;//cmd topic
+	std::string						_password; //	+k
+	int								_userLimit;//	+l
+
+	std::set<Client*>				_members;
+	std::set<Client*>				_operators;//+o
+	std::set<Client*>				_invited; // +i
+
+	bool							_inviteOnly; // +i
+	bool							_topicRestricted; // +t
+	bool							_passwordProtected; // +k
+	bool							_limitUsers; // +l
+
 public:
-    Channel(/* args */);
-    ~Channel();
+	Channel(const std::string name);
+	~Channel();
+
+	const std::string& getName() const;
+	void addMember(Client* client);
+	bool isMember(Client* client) const;
 };
 
 #endif
--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -1,10 +1,23 @@
 #include "Channel.hpp"
 
-Channel::Channel(/* args */)
-{
+Channel::Channel(std::string name):
+	_name(name), _topic(""), _password(""),
+	_userLimit(0), _inviteOnly(false),
+	_topicRestricted(false), _passwordProtected(false),
+	_limitUsers(false) {}
+
+Channel::~Channel() {
+
 }
 
-Channel::~Channel()
-{
+const std::string& Channel::getName() const {
+	return _name;
 }
 
+void Channel::addMember(Client* client){
+	_members.insert(client);
+}
+
+bool Channel::isMember(Client* client) const{
+	return _members.find(client) != _members.end();
+}
