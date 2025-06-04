0933015855e6a55201f377d21b88e30de06fee1a
socallmebertille <sarah.bejard@gmail.com>
2025-06-04 15:43:14 +0200

    try to init 3 cmd

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -10,6 +10,9 @@ const std::string& Client::getUsername() const{ return _username; }
 const std::string& Client::getNickname() const{ return _nickname; }
 const std::string& Client::getRealname() const{ return _realName; }
 const std::string& Client::getPassword() const { return _password; }
+bool Client::hasPassword() const { return _hasPassword; }
+bool Client::hasNick() const { return _hasNick; }
+bool Client::hasUser() const { return _hasUser; }
 
 bool Client::setNickname(const std::string& nick){
 	if (nick.empty())
@@ -22,10 +25,18 @@ bool Client::setNickname(const std::string& nick){
 void Client::setUsername(const std::string& user){ _username = user; }
 void Client::setRealname(const std::string& real) {_realName = real; }
 void Client::setPassword(const std::string& passW) { _password = passW;}
+void Client::markPassword() { _hasPassword = true; }
+void Client::markNick() { _hasNick = true; }
+void Client::markUser() { _hasUser = true; }
 
 bool Client::isAuthenticated() const { return _authenticated; }
 
-void Client::authenticate() { _authenticated = true; }
+void Client::authenticate() {
+	if (!_password.empty() && !_nickname.empty() && !_username.empty())
+    	_authenticated = true;
+	else
+		_authenticated = false;
+}
 
 void Client::registerUser(const std::string& nick, const std::string& user, const std::string& real) {
 	setNickname(nick);
