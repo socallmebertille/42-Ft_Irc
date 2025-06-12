8973fee9393363bd012cdf8d12d4caff4d89cfe9
socallmebertille <sarah.bejard@gmail.com>
2025-06-12 17:24:43 +0200

    i think part 2 jira done

--- srcs/Commands.cpp
+++ srcs/Commands.cpp
@@ -11,29 +11,29 @@ void Server::cap() {
     iss >> subcmd;
 
     if (subcmd == "LS") {
-        sendToClient(_clientFd, ":localhost CAP * LS :\r\n");
-        sendToClient(_clientFd, ":localhost CAP * END\r\n");
+        sendToClient(_clientFd, ":localhost CAP * LS :");
+        sendToClient(_clientFd, ":localhost CAP * END");
     }
     else if (subcmd == "REQ") {
-        sendToClient(_clientFd, ":localhost CAP * NAK :\r\n");
-        sendToClient(_clientFd, ":localhost CAP * END\r\n");
+        sendToClient(_clientFd, ":localhost CAP * NAK :");
+        sendToClient(_clientFd, ":localhost CAP * END");
     }
     else if (subcmd == "LIST") {
-        sendToClient(_clientFd, ":localhost CAP * LIST :\r\n");
-        sendToClient(_clientFd, ":localhost CAP * END\r\n");
+        sendToClient(_clientFd, ":localhost CAP * LIST :");
+        sendToClient(_clientFd, ":localhost CAP * END");
     }
     else if (subcmd == "END") {
-        sendToClient(_clientFd, ":localhost CAP * END\r\n");
+        sendToClient(_clientFd, ":localhost CAP * END");
     }
 }
 
 
 void Server::ping() {
 	// if (_client->getArg().empty()) {
-	// 	sendToClient(_clientFd, "409 :No origin specified\r\n");
+	// 	sendToClient(_clientFd, "409 :No origin specified");
 	// 	return;
 	// }
-	// std::string response = "PONG :" + _client->getArg() + "\r\n";
+	// std::string response = "PONG :" + _client->getArg();
 	// sendToClient(_clientFd, response);
 	return;
 }
@@ -44,11 +44,11 @@ void Server::pong() {
 
 void Server::pass() {
 	if (_client->isRegistered()) {
-		sendToClient(_clientFd, "462 :You may not reregister\r\n");
+		sendToClient(_clientFd, "462 :You may not reregister");
 		return;
 	}
 	if (_client->getArg().empty()) {
-		sendToClient(_clientFd, "461 PASS :Not enough parameters\r\n");
+		sendToClient(_clientFd, "461 PASS :Not enough parameters");
 		return;
 	}
 	std::string inputPass = _client->getArg();
@@ -57,14 +57,14 @@ void Server::pass() {
 		_client->setPasswordOk(true);
 		_client->setPassErrorSent(false);
 	} else {
-		sendToClient(_clientFd, "464 :Password incorrect\r\n");
+		sendToClient(_clientFd, "464 :Password incorrect");
 		_client->setPasswordOk(false);
 	}
 }
 
 void Server::nick() {
 	if (_client->getArg().empty()) {
-		sendToClient(_clientFd, "431 * :No nickname given\r\n");
+		sendToClient(_clientFd, "431 * :No nickname given");
 		return;
 	}
 	std::string newNick = _client->getArg();
@@ -73,20 +73,20 @@ void Server::nick() {
 		newNick = newNick.substr(0, firstSpace);
 	for (std::map<int, Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it) {
 		if (it->second->getNickname() == newNick && it->first != _clientFd) {
-			sendToClient(_clientFd, "433 * " + newNick + " :Nickname is already in use\r\n");
+			sendToClient(_clientFd, "433 * " + newNick + " :Nickname is already in use");
 			return;
 		}
 	}
 	std::string oldNick = _client->getNickname();
 	_client->setNickname(newNick);
 	std::string prefix = oldNick.empty() ? "*" : oldNick;
-	std::string nickMsg = ":" + prefix + "!" + (_client->getUsername().empty() ? "*" : _client->getUsername()) + "@localhost NICK :" + newNick + "\r\n";
+	std::string nickMsg = ":" + prefix + "!" + (_client->getUsername().empty() ? "*" : _client->getUsername()) + "@localhost NICK :" + newNick;
 	sendToClient(_clientFd, nickMsg);
 }
 
 void Server::user() {
 	if (_client->isRegistered()) {
-		sendToClient(_clientFd, "462 :You may not reregister\r\n");
+		sendToClient(_clientFd, "462 :You may not reregister");
 		return;
 	}
 	std::string args = _client->getArg();
@@ -101,13 +101,13 @@ void Server::user() {
 	std::istringstream iss(args);
 	std::string username, hostname, servername, realname;
 	if (!(iss >> username >> hostname >> servername)) {
-		sendToClient(_clientFd, "461 USER :Not enough parameters\r\n");
+		sendToClient(_clientFd, "461 USER :Not enough parameters");
 		return;
 	}
 	if (realNamePos != std::string::npos) {
 		realname = args.substr(realNamePos + 2);
 	} else {
-		sendToClient(_clientFd, "461 USER :Not enough parameters\r\n");
+		sendToClient(_clientFd, "461 USER :Not enough parameters");
 		return;
 	}
 	_client->setUsername(username);
@@ -132,7 +132,7 @@ void Server::privmsg() {
 		std::istringstream iss(_client->getBuffer());
 		std::getline(iss, message);
 		if (message.empty() || message[0] != ':') {
-			sendToClient(_clientFd, "412 :No text to send\r\n");
+			sendToClient(_clientFd, "412 :No text to send");
 			_client->eraseBuf();
 			return;
 		}
@@ -140,27 +140,27 @@ void Server::privmsg() {
 	}
 	Client* target = getClientByNick(_client->getArg());
 	if (!target) {
-		sendToClient(_clientFd, "401 " + _client->getArg() + " :No such nick/channel\r\n");
+		sendToClient(_clientFd, "401 " + _client->getArg() + " :No such nick/channel");
 		_client->eraseBuf();
 		return;
 	}
 	if (message[0] == ' ')
 		message.erase(0, 1);
-	std::string fullMsg = ":" + _client->getPrefix() + " PRIVMSG " + target->getNickname() + " :" + message + "\r\n";
+	std::string fullMsg = ":" + _client->getPrefix() + " PRIVMSG " + target->getNickname() + " :" + message;
 	sendToClient(target->getFd(), fullMsg);
 	_client->eraseBuf();
 }
 
 void Server::join() {
 	if (_client->getArg().empty() || _client->getArg()[0] != '#') {
-		sendToClient(_clientFd, "ERROR :Invalid channel name\r\n");
+		sendToClient(_clientFd, "ERROR :Invalid channel name");
 		return;
 	}
 	std::pair<std::map<std::string, Channel>::iterator, bool> result = _channels.insert(std::make_pair(_client->getArg(), Channel(_client->getArg())));
 	Channel& chan = result.first->second;
 	if (!chan.isMember(_client)) {
 		chan.join(_client);
-		std::string joinMsg = ":" + _client->getPrefix() + " JOIN :" + _client->getArg() + "\r\n";
+		std::string joinMsg = ":" + _client->getPrefix() + " JOIN :" + _client->getArg();
 		const std::set<Client*>& members = chan.getMembers();
 		for (std::set<Client*>::const_iterator it = members.begin(); it != members.end(); ++it)
 			sendToClient((*it)->getFd(), joinMsg);
@@ -168,26 +168,26 @@ void Server::join() {
 		for (std::set<Client*>::const_iterator it = members.begin(); it != members.end(); ++it)
 			nickList += (*it)->getNickname() + " ";
 		sendReply(353, _client, "=", _client->getArg(), nickList);
-		sendReply(366, _client, _client->getArg(), "", "End of /NAMES list.\r\n");
+		sendReply(366, _client, _client->getArg(), "", "End of /NAMES list.");
 	}
 }
 
 void Server::part() {
 	if (_client->getArg().empty() || _client->getArg()[0] != '#') {
-		sendToClient(_clientFd, "ERROR :Invalid channel name\r\n");
+		sendToClient(_clientFd, "ERROR :Invalid channel name");
 		return;
 	}
 	std::map<std::string, Channel>::iterator it = _channels.find(_client->getArg());
 	if (it == _channels.end()) {
-		sendToClient(_clientFd, "ERROR :No such channel\r\n");
+		sendToClient(_clientFd, "ERROR :No such channel");
 		return;
 	}
 	Channel& chan = it->second;
 	if (!chan.isMember(_client)) {
-		sendToClient(_clientFd, "ERROR :You're not in that channel\r\n");
+		sendToClient(_clientFd, "ERROR :You're not in that channel");
 		return;
 	}
-	std::string partMsg = ":" + _client->getPrefix() + " PART " + _client->getArg() + "\r\n";
+	std::string partMsg = ":" + _client->getPrefix() + " PART " + _client->getArg();
 	const std::set<Client*>& members = chan.getMembers();
 	for (std::set<Client*>::const_iterator it = members.begin(); it != members.end(); ++it)
 		sendToClient((*it)->getFd(), partMsg);
@@ -203,10 +203,10 @@ void Server::quit() {
 
 void Server::mode() {
 	if (_client->getArg().empty()) {
-		sendToClient(_clientFd, "461 MODE :Not enough parameters\r\n");
+		sendToClient(_clientFd, "461 MODE :Not enough parameters");
 		return;
 	}
-	sendToClient(_clientFd, "324 " + _client->getNickname() + " +\r\n");
+	sendToClient(_clientFd, "324 " + _client->getNickname());
 }
 
 void Server::topic() {
